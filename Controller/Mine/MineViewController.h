1e7a9f29229e24db2026034571b45a568006cb46
d191562687 <191562687@qq.com>
2016-08-23 17:56:51 +0800

    Mine
    Mine

---
+++ Controller/Mine/MineViewController.h
@@ -0,0 +1,13 @@
+//
+//  MineViewController.h
+//  JinAnJian
+//
+//  Created by 金安健 on 16/8/23.
+//  Copyright © 2016年 JinAnJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MineViewController : UIViewController
+
+@end
---
+++ Controller/Mine/MineViewController.m
@@ -0,0 +1,13 @@
+//
+//  MineViewController.m
+//  JinAnJian
+//
+//  Created by 金安健 on 16/8/23.
+//  Copyright © 2016年 JinAnJian. All rights reserved.
+//
+
+#import "MineViewController.h"
+
+@implementation MineViewController
+
+@end
